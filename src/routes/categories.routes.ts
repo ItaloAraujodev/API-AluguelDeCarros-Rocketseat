ab8c0894ec965b0f8b0f72b381c3ae417e529771
italo21211 <italoarauju2019@gmail.com>
2022-11-06 17:16:54 -0300

    Configurando o Multer e criando UseCase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,8 +2,14 @@ import { Router } from 'express';
 import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
+import multer from 'multer'
 
 const categoriesRoutes = Router();
+const upload = multer({
+    dest: "./tmp",
+});
+
 // Instanciando
 const categoriesRepository = CategoriesRepository.getInstance();
 
@@ -13,6 +19,10 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
     return listCategoriesController.handle(request, response)
+});
+
+categoriesRoutes.post('/import', upload.single("file"), (request, response) => {
+    return importCategoryController.handle(request, response)
 });
 
 
