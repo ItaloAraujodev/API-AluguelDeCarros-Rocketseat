f307096c1c461253289fe81c69fca4dbb32d9321
italo21211 <italoarauju2019@gmail.com>
2022-11-06 18:00:58 -0300

    Tentando ler um arquivo em pdf

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -1,7 +1,18 @@
+import fs from "fs";
+import { PdfReader } from "pdfreader";
+
 class ImportCategoryUseCase {
 
-    execute(file: any){
-        console.log(file);
+    execute(file: Express.Multer.File): void{
+       /* const stream = fs.readFileSync(file.path); */
+        const pdf = new PdfReader().parseFileItems(file.path, (err: any, item:any) => {
+                if (err) console.error("error:", err);
+                else if (!item) console.warn("end of file");
+                else if (item.text) console.log(item.text);
+              });
+        
+
+       console.log(pdf)
     }
 }
 
